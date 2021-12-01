ba9785e9e088d3965fb7d6c4edece73813370120
Get-R1Ght99 <95370830+Get-R1Ght99@users.noreply.github.com>
2021-12-01 19:51:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var preloader = document.getElementById("preloader");
+var preloader_svg = document.getElementById("preloader_svg");
+
+window.onload = function() {
+    preloader.classList.add("loaded");
+    preloader_svg.classList.add("loaded");
+}
